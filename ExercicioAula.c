7915f33f08cf57f66747b107fc38df0f2156964a
Augustofsp <73397414+Augustofsp@users.noreply.github.com>
2020-11-03 19:11:17 +0000

    Exercicio aula

---
+++ ExercicioAula.c
@@ -0,0 +1,45 @@
+/**
+ * @file main.c
+ * @author Augusto Pereira (a21136@alunos.ipca.pt)
+ * @brief Soma, subtração, multiplicação e divisão de 2 números inteiros
+ * @version 0.1
+ * @date 2020-11-03
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * @brief Soma, subtração, multiplicação e divisão de 2 números inteiros
+ * 
+ */
+
+int main()
+{
+    //Variaveis
+    int numero1, numero2,soma, subtracao, multiplicacao, divisao;
+
+    //Inserir dados
+    puts("Calculo de varias operacoes com 2 numeros inteiros\n");
+    printf("Insira o primeiro numero: ");
+    scanf("%d", &numero1);
+    printf("Insira o segundo numero: ");
+    scanf("%d", &numero2);
+
+    //Calculo das diversas operacoes
+    soma = numero1 + numero2;
+    subtracao = numero1 - numero2;
+    multiplicacao = numero1 * numero2;
+    divisao = numero1 / numero2;
+
+    //Escrever o resultado das operacoes
+    printf("O resultado da soma e: %d\n", soma);
+    printf("O resultado da subtracao e: %d\n", subtracao);
+    printf("O resultado da multiplicacao e: %d\n", multiplicacao);
+    printf("O resultado da divisao e: %d\n", divisao);
+
+    getchar();
+    getchar();
+    return 0;
+}
